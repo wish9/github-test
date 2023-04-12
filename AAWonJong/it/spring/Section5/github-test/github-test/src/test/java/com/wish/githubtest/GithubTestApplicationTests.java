ae96d70ef3b19ee9c9b1dc898a301b0c49bffe4a
wish9 <wjwee9@gmail.com>
2023-04-12 11:18:24 +0900

    initial

---
+++ AAWonJong/it/spring/Section5/github-test/github-test/src/test/java/com/wish/githubtest/GithubTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wish.githubtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
